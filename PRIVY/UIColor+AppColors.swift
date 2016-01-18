068bbe0d407f6e750d691bc04e621d44f2cca0ea
Michael MacCallum <micksmaccallum@gmail.com>
2016-01-17 22:23:06 -0500

    Added UIColor extension for application colors

---
+++ PRIVY/UIColor+AppColors.swift
@@ -0,0 +1,29 @@
+//
+//  UIColor+AppColors.swift
+//  PRIVY
+//
+//  Created by Michael MacCallum on 1/17/16.
+//  Copyright © 2016 Michael MacCallum. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static func appLightBlueColor() -> UIColor {
+        return UIColor(
+            red: 35.0 / 255.0,
+            green: 95.0 / 255.0,
+            blue: 190.0 / 255.0,
+            alpha: 1.0
+        )
+    }
+
+    static func appDarkBlueColor() -> UIColor {
+        return UIColor(
+            red: 3.0 / 255.0,
+            green: 33.0 / 255.0,
+            blue: 85.0 / 255.0,
+            alpha: 1.0
+        )
+    }
+}
