413c488a9b52c74fa5b87e964f84df7dea22b1d3
Michael MacCallum <micksmaccallum@gmail.com>
2016-02-23 09:48:06 -0500

    Basic QR generation and tracking for Exchange VC

--- PRIVY/ExchangeViewController.swift
+++ PRIVY/ExchangeViewController.swift
@@ -9,45 +9,150 @@
 import UIKit
 import DynamicButton
 import NVActivityIndicatorView
+import AVFoundation
 
 class ExchangeViewController: UIViewController {
-    @IBOutlet private weak var closeButton: DynamicButton! {
-        didSet {
-            closeButton.setStyle(DynamicButton.Style.Close, animated: false)
+    private let captureSession = AVCaptureSession()
+    private let captureOutput = AVCaptureMetadataOutput()
+    private let captureCallbackQueue = dispatch_queue_create("com.Privy.qrScan", DISPATCH_QUEUE_SERIAL)
 
-            closeButton.lineWidth           = 2
-            closeButton.strokeColor         = UIColor.privyLightBlueColor
-            closeButton.highlightStokeColor = UIColor.whiteColor()
-            closeButton.backgroundColor     = UIColor.whiteColor()
-            closeButton.layer.cornerRadius  = closeButton.bounds.width / 2.0
-            closeButton.layer.masksToBounds = true
+    private var trackingMetadataObject: AVMetadataMachineReadableCodeObject?
+    
+    private let infoSwappingQueue = NSOperationQueue()
+    private var qrGenOperation: QRGeneratorOperation {
+        return QRGeneratorOperation(
+            qrString: PrivyUser.currentUser.qrString,
+            size: self.qrCodeImageView?.bounds.size ?? CGSize(width: 151, height: 151),
+            scale: UIScreen.mainScreen().scale,
+            correctionLevel: .Medium) { (image) in
+                dispatch_async(dispatch_get_main_queue()) { [weak self] in
+                    self?.qrCodeImage = image
+                }
         }
     }
     
-    @IBOutlet private weak var loadingIndicator: NVActivityIndicatorView! {
+    private var qrCodeImage: UIImage? {
         didSet {
-            loadingIndicator.type = NVActivityIndicatorType.BallClipRotateMultiple
-            loadingIndicator.hidesWhenStopped = true
-            loadingIndicator.size = loadingIndicator.bounds.size
+            qrCodeImageView?.image = qrCodeImage
         }
     }
-
+    
+    @IBOutlet private weak var qrCodeImageView: UIImageView! {
+        didSet {
+            qrCodeImageView?.image = qrCodeImage
+        }
+    }
+    
+    @IBOutlet private weak var captureOutputView: CaptureLayerView!
+    @IBOutlet private weak var captureOutlineView: OutlinedTransformableView!
+    @IBOutlet private weak var toggleCameraButton: UIButton!
+    
+    private weak var capturePreviewLayer: AVCaptureVideoPreviewLayer! {
+        return captureOutputView.layer as! AVCaptureVideoPreviewLayer
+    }
+    
+    private var qrTimer: NSTimer?
+//    @IBOutlet private weak var closeButton: DynamicButton! {
+//        didSet {
+//            closeButton.setStyle(DynamicButton.Style.Close, animated: false)
+//
+//            closeButton.lineWidth           = 2
+//            closeButton.strokeColor         = UIColor.privyLightBlueColor
+//            closeButton.highlightStokeColor = UIColor.whiteColor()
+//            closeButton.backgroundColor     = UIColor.whiteColor()
+//            closeButton.layer.cornerRadius  = closeButton.bounds.width / 2.0
+//            closeButton.layer.masksToBounds = true
+//        }
+//    }
+    
+//    @IBOutlet private weak var loadingIndicator: NVActivityIndicatorView! {
+//        didSet {
+//            loadingIndicator.type = NVActivityIndicatorType.BallClipRotateMultiple
+//            loadingIndicator.hidesWhenStopped = true
+//            loadingIndicator.size = loadingIndicator.bounds.size
+//        }
+//    }
+    
+    override required init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        infoSwappingQueue.addOperation(qrGenOperation)
+        
+        #if os(iOS) && !(arch(i386) || arch(x86_64))
+        guard let captureDevice = AVCaptureDevice.frontCamera() ?? AVCaptureDevice.defaultDeviceWithMediaType(.Video) else {
+            return
+        }
+        
+        do {
+            let deviceInput = try AVCaptureDeviceInput(device: captureDevice)
+        
+            if captureSession.canAddInput(deviceInput) {
+                captureSession.addInput(deviceInput)
+            } else {
+                print("adding input failed")
+            }
+        } catch {
+            return
+        }
+        #endif
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        #if os(iOS) && !(arch(i386) || arch(x86_64))
+        capturePreviewLayer.session = captureSession
+        capturePreviewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+
+        if captureSession.canSetSessionPreset(AVCaptureSessionPresetPhoto) {
+            captureSession.sessionPreset = AVCaptureSessionPresetPhoto
+        }
+        
+        if captureSession.canAddOutput(captureOutput) {
+            captureSession.addOutput(captureOutput)
+            
+            captureOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+            captureOutput.setMetadataObjectsDelegate(self, queue: captureCallbackQueue)
+        }
+        #endif
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        #if os(iOS) && !(arch(i386) || arch(x86_64))
+        captureSession.startRunning()
+        
+        let front = AVCaptureDevice.frontCamera()
+        let back = AVCaptureDevice.backCamera()
+        
+        toggleCameraButton.hidden = front == nil || back == nil
+        #else
+        toggleCameraButton.hidden = true
+        #endif
     }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-        
-        loadingIndicator.startAnimation()
     }
     
     override func viewDidDisappear(animated: Bool) {
         super.viewDidDisappear(animated)
-        
-        loadingIndicator.stopAnimation()
+
+        captureSession.stopRunning()
+        qrTimer?.invalidate()
     }
 
     override func didReceiveMemoryWarning() {
@@ -60,9 +165,58 @@ class ExchangeViewController: UIViewController {
     }
     
     override func preferredStatusBarUpdateAnimation() -> UIStatusBarAnimation {
-        return .Slide
+        return .Fade
     }
 
+    @objc private func qrTimerFired(timer: NSTimer) {
+        captureOutlineView.hidden = true
+    }
+    
+    @IBAction private func toggleCameraButtonTapped(button: UIButton) {
+        #if os(iOS) && !(arch(i386) || arch(x86_64))
+        captureSession.beginConfiguration()
+
+        let deviceInputs = captureSession.inputs.flatMap { $0 as? AVCaptureDeviceInput }
+        
+        var newPosition = AVCaptureDevicePosition.Unspecified
+        for deviceInput in deviceInputs {
+            if deviceInput.device.position == .Front {
+                newPosition = .Back
+                break
+            }
+            
+            if deviceInput.device.position == .Back {
+                newPosition = .Front
+                break
+            }
+        }
+
+        captureSession.removeInput(captureSession.inputs.first as! AVCaptureInput)
+
+        guard let captureDevice = newPosition == .Front ? AVCaptureDevice.frontCamera() : AVCaptureDevice.backCamera() else {
+            return
+        }
+        
+        do {
+            let deviceInput = try AVCaptureDeviceInput(device: captureDevice)
+            
+            if captureSession.canAddInput(deviceInput) {
+                captureSession.addInput(deviceInput)
+            } else {
+                print("adding input failed")
+            }
+        } catch {
+            return
+        }
+
+        if captureSession.canSetSessionPreset(AVCaptureSessionPresetPhoto) {
+            captureSession.sessionPreset = AVCaptureSessionPresetPhoto
+        }
+
+        captureSession.commitConfiguration()
+        #endif
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -74,3 +228,46 @@ class ExchangeViewController: UIViewController {
     */
 
 }
+
+extension ExchangeViewController: AVCaptureMetadataOutputObjectsDelegate {
+    func captureOutput(captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, fromConnection connection: AVCaptureConnection!) {
+        
+        let readableObjects = metadataObjects.flatMap { $0 as? AVMetadataMachineReadableCodeObject }
+        
+        for readableObject in readableObjects where readableObject.type == AVMetadataObjectTypeQRCode && readableObject.stringValue != nil {
+            dispatch_async(dispatch_get_main_queue()) {
+                self.detectedReadableObject(readableObject)
+            }
+        }
+    }
+    
+    private func detectedReadableObject(object: AVMetadataMachineReadableCodeObject) {
+        trackingMetadataObject = object
+        
+        guard let transformed = capturePreviewLayer.transformedMetadataObjectForMetadataObject(object) as? AVMetadataMachineReadableCodeObject else {
+            return
+        }
+        
+        captureOutlineView.hidden = false
+        captureOutlineView.corners = translatePoints(
+            transformed.corners as! [NSDictionary]
+        )
+        
+        if qrTimer?.valid ?? false {
+            qrTimer?.invalidate()
+        }
+        
+        qrTimer = NSTimer.scheduledTimerWithTimeInterval(0.05, target: self, selector: "qrTimerFired:", userInfo: nil, repeats: false)
+    }
+    
+    private func translatePoints(points: [NSDictionary]) -> [CGPoint] {
+        return points.map {
+            CGPoint(x: $0.objectForKey("X")!.doubleValue!, y: $0.objectForKey("Y")!.doubleValue!)
+        }
+    }
+}
+
+
+
+
+
