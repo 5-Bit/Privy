b09fb888a31bb1f0377150d824374635e7ea676d
Michael MacCallum <micksmaccallum@gmail.com>
2016-02-28 11:19:45 -0500

    Commented AVCaptureDevice front/back cameras extension

--- PRIVY/AVCaptureDevice+Cameras.swift
+++ PRIVY/AVCaptureDevice+Cameras.swift
@@ -9,14 +9,33 @@
 import AVFoundation
 
 extension AVCaptureDevice {
+    
+    /**
+     Enumerates all the available capture devices, attempting to get the one representing the front camera.
+     
+     - returns: An AVCaptureDevice representing the front camera, if one exists, otherwise nil.
+     */
     class func frontCamera() -> AVCaptureDevice? {
         return firstDeviceAtPosition(.Front, forMediaType: .Video)
     }
 
+    /**
+     Enumerates all the available capture devices, attempting to get the one representing the back camera.
+     
+     - returns: An AVCaptureDevice representing the back camera, if one exists, otherwise nil.
+     */
     class func backCamera() -> AVCaptureDevice? {
         return firstDeviceAtPosition(.Back, forMediaType: .Video)
     }
     
+    /**
+     Enumerates all the available capture devices, attempting to fine one that matches the input criteria.
+     
+     - parameter position:  The camera position to search for (e.g. .Front, .Back)
+     - parameter mediaType: The media type that the camera at the given position should support.
+     
+     - returns: The first AVCapture device found that matches the given criteria, if none exists, nil.
+     */
     private class func firstDeviceAtPosition(position: AVCaptureDevicePosition, forMediaType mediaType: AVMediaType) -> AVCaptureDevice? {
         let devices = devicesWithMediaType(mediaType) as! [AVCaptureDevice]
         
