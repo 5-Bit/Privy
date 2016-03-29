538c994072dacb80c79b82160b87d10b4efeafee
Michael MacCallum <micksmaccallum@gmail.com>
2016-03-28 22:34:06 -0400

    refactor

--- PRIVY/ServerResponses.swift
+++ PRIVY/ServerResponses.swift
@@ -17,15 +17,10 @@ struct LoginRegistrationResponse: Mappable {
     var developer: String?
     var media: String?
     var blogging: String?
+    var email: String?
     
     init?(_ map: Map) {
-        sessionid   <-  map["sessionid"]
-        basic       <-  map["basic"]
-        social      <-  map["social"]
-        business    <-  map["business"]
-        developer   <-  map["developer"]
-        media       <-  map["media"]
-        blogging    <-  map["blogging"]
+        mapping(map)
     }
     
     mutating func mapping(map: Map) {
@@ -36,12 +31,12 @@ struct LoginRegistrationResponse: Mappable {
         developer   <-  map["developer"]
         media       <-  map["media"]
         blogging    <-  map["blogging"]
+        email       <-  map["email"]
     }
-    
+
     var valid: Bool {
         return sessionid != nil && basic != nil && social != nil
             && business != nil && developer != nil && media != nil
             && blogging != nil
     }
-    
 }
