e069e288cd1e1303232981ad375247e251cff32e
Michael MacCallum <micksmaccallum@gmail.com>
2016-03-31 17:55:13 -0400

    Refactor, Equatable for PrivyErrorStatus type

--- PRIVY/RequestManager.swift
+++ PRIVY/RequestManager.swift
@@ -13,8 +13,17 @@ enum HttpMethod: String {
     case POST, GET, PUT, PATCH, DELETE
 }
 
-enum PrivyErrorStatus: ErrorType {
+enum PrivyErrorStatus: ErrorType, Equatable {
     case Ok, ServerError(String), NoResponse, UnknownError
+}
+
+func == (lhs: PrivyErrorStatus, rhs: PrivyErrorStatus) -> Bool {
+    switch (lhs, rhs) {
+    case (.Ok, .Ok), (.ServerError(_), .ServerError(_)), (.NoResponse, .NoResponse), (.UnknownError, .UnknownError):
+        return true
+    default:
+        return false
+    }
 }
 
 enum PrivyHttpHeaderField: String {
@@ -57,38 +66,57 @@ final class RequestManager {
         session = NSURLSession(configuration: configuration)
     }
 
-    private func refreshUUIDs(uuids: [String], completion: (success: Bool) -> Void) {
-        // comma separated uuids in body uuids=comma,comma    GET
-        // returns object containing PrivyUser.InfoTypes
-        let url = Static.host.URLByAppendingPathComponent("users/refreshinfo")
+    /**
+     <#Description#>
 
-        let queryItems = [
-            NSURLQueryItem(name: "uuids", value: uuids.joinWithSeparator(",")),
+     - parameter completion: <#completion description#>
+     */
+    func refreshHistory(completion: (history: [HistoryUser]?, errorStatus: PrivyErrorStatus) -> Void) {
+        let queryItems: [NSURLQueryItem] = [
             NSURLQueryItem(name: "sessionid", value: PrivyUser.currentUser.userInfo.sessionid)
         ]
 
-        let body = url.urlByAppendingQueryItems(queryItems).query?.dataUsingEncoding(NSUTF8StringEncoding)
+        let url = Static.host
+            .URLByAppendingPathComponent("users/refresh")
+            .urlByAppendingQueryItems(queryItems)
 
-        handleRequest(url, method: .POST, body: body) { _, response, error in
-            var success = false
+        handleRequest(url, method: .GET) { data, response, error in
+            var errorStatus = PrivyErrorStatus.Ok
+            var history: [HistoryUser]?
 
             defer {
-                self.completionOnMainThread(success, completion: completion)
+                self.completionOnMainThread(history, errorStatus: errorStatus, completion: completion)
             }
 
-            guard let response = response as? NSHTTPURLResponse
-                where error == nil else {
-                    return
-            }
-
-            guard response.statusCode == 200 else {
+            guard let response = response as? NSHTTPURLResponse else {
+                errorStatus = .NoResponse
                 return
             }
 
-            success = true
+            switch response.statusCode {
+            case 200:
+                if let data = data,
+                    jsonString = String(data: data, encoding: NSUTF8StringEncoding),
+                    users = Mapper<HistoryUser>().mapArray(jsonString) {
+
+                    history = users
+                } else {
+                    errorStatus = .UnknownError
+                }
+            case 400, 405, 500..<600:
+                errorStatus = .ServerError(response.allHeaderFields[PrivyHttpHeaderField.Error.rawValue] as? String ?? "")
+            default:
+                errorStatus = .UnknownError
+            }
         }
     }
 
+    /**
+     <#Description#>
+
+     - parameter token:      <#token description#>
+     - parameter completion: <#completion description#>
+     */
     func addApnsToken(token: NSData, completion: (success: Bool) -> Void) {
         let url = Static.host.URLByAppendingPathComponent("users/registerapnsclient")
 
@@ -118,7 +146,13 @@ final class RequestManager {
         }
     }
 
-    func attemptLookupByUUIDs(uuids: [String], completion: (user: PrivyUser.InfoTypes?, errorStatus: PrivyErrorStatus) -> Void) {
+    /**
+     <#Description#>
+
+     - parameter uuids:      <#uuids description#>
+     - parameter completion: <#completion description#>
+     */
+    func attemptLookupByUUIDs(uuids: [String], completion: (user: InfoTypes?, errorStatus: PrivyErrorStatus) -> Void) {
         guard let sessionId = PrivyUser.currentUser.registrationInformation?.sessionid else {
             return
         }
@@ -131,7 +165,7 @@ final class RequestManager {
         let url = RequestManager.Static.host.URLByAppendingPathComponent("/users/info").urlByAppendingQueryItems(queryItems)
 
         handleRequest(url) { (data, response, error) in
-            var infoTypes: PrivyUser.InfoTypes?
+            var infoTypes: InfoTypes?
             var errorStatus = PrivyErrorStatus.Ok
 
             defer {
@@ -147,7 +181,7 @@ final class RequestManager {
             case 200:
                 if let data = data,
                     jsonString = String(data: data, encoding: NSUTF8StringEncoding),
-                    registerResponse = Mapper<PrivyUser.InfoTypes>().map(jsonString) {
+                    registerResponse = Mapper<InfoTypes>().map(jsonString) {
 
                     infoTypes = registerResponse
                 } else {
@@ -161,6 +195,11 @@ final class RequestManager {
         }
     }
 
+    /**
+     <#Description#>
+
+     - returns: <#return value description#>
+     */
     private func userJsonData() -> NSData? {
         PrivyUser.currentUser.userInfo.sessionid = PrivyUser.currentUser.registrationInformation?.sessionid
 
@@ -168,10 +207,13 @@ final class RequestManager {
             return nil
         }
 
-        let jsonString = Mapper<PrivyUser.InfoTypes>().toJSONString(PrivyUser.currentUser.userInfo)
+        let jsonString = Mapper<InfoTypes>().toJSONString(PrivyUser.currentUser.userInfo)
         return jsonString?.dataUsingEncoding(NSUTF8StringEncoding)
     }
 
+    /**
+     <#Description#>
+     */
     func attemptUserInfoSave() {
         guard let userData = userJsonData() else {
             return
@@ -195,7 +237,14 @@ final class RequestManager {
     func attemptLoginWithCredentials(credential: LoginCredential, completion: LoginCompletion) {
         registerLoginWithPath("users/login", credential: credential, completion: completion)
     }
-    
+
+    /**
+     <#Description#>
+
+     - parameter pathComponent: <#pathComponent description#>
+     - parameter credential:    <#credential description#>
+     - parameter completion:    <#completion description#>
+     */
     private func registerLoginWithPath(pathComponent: String, credential: LoginCredential, completion: LoginCompletion) {
         let queryItems = [
             NSURLQueryItem(name: "email", value: credential.email),
@@ -270,12 +319,20 @@ final class RequestManager {
         )
 
         request.method = method
-        request.HTTPBody = body
+        if let body = body {
+            request.HTTPBody = body
+        }
 
         session.dataTaskWithRequest(request, completionHandler: completion).resume()
     }
 
-    private func completionOnMainThread(user: PrivyUser.InfoTypes?, errorStatus: PrivyErrorStatus, completion: (user: PrivyUser.InfoTypes?, errorStatus: PrivyErrorStatus) -> Void) {
+    private func completionOnMainThread(history: [HistoryUser]?, errorStatus: PrivyErrorStatus, completion: (history: [HistoryUser]?, errorStatus: PrivyErrorStatus) -> Void) {
+        dispatch_async(dispatch_get_main_queue()) {
+            completion(history: history, errorStatus: errorStatus)
+        }
+    }
+
+    private func completionOnMainThread(user: InfoTypes?, errorStatus: PrivyErrorStatus, completion: (user: InfoTypes?, errorStatus: PrivyErrorStatus) -> Void) {
         dispatch_async(dispatch_get_main_queue()) {
             completion(user: user, errorStatus: errorStatus)
         }
