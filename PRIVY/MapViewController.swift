b08c07cd08150d9c6e81eb86b5d0af8cc0929b68
Michael MacCallum <micksmaccallum@gmail.com>
2016-04-16 18:24:32 -0400

    Functionality for map view
    still needs to drill down, but….

--- PRIVY/MapViewController.swift
+++ PRIVY/MapViewController.swift
@@ -11,19 +11,52 @@ import MapKit
 
 class MapViewController: UIViewController {
     @IBOutlet private weak var mapView: MKMapView!
-    
+
+    var allUsers = [HistoryUser]()
+    var currentUserIndex: Int?
+    private var currentUserAnnotation: MKPointAnnotation?
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        if allUsers.count == 1 {
+            title = "Showing \(allUsers.count) contact"
+        } else {
+            title = "Showing \(allUsers.count) contacts"
+        }
+
+        for (index, user) in allUsers.enumerate() where user.location != nil && user.location!.latitude != nil && user.location!.longitude != nil {
+            let annotation = MKPointAnnotation()
+
+            annotation.coordinate = CLLocationCoordinate2D(
+                latitude: user.location!.latitude!,
+                longitude: user.location!.longitude!
+            )
+
+            var names = [String?]()
+            names.append(user.basic.firstName)
+            names.append(user.basic.lastName)
+
+            annotation.title = names.flatMap({ $0 }).joinWithSeparator(" ")
+
+            if let currentUser = currentUserIndex where index == currentUser {
+                currentUserAnnotation = annotation
+            }
+
+            mapView.addAnnotation(annotation)
+        }
+    }
+
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
+
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -31,10 +64,22 @@ class MapViewController: UIViewController {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
-
 }
 
 extension MapViewController: MKMapViewDelegate {
-    
+    func mapView(mapView: MKMapView, didAddAnnotationViews views: [MKAnnotationView]) {
+        var zoomRect = MKMapRectNull
+
+        for view in views where view.annotation != nil {
+            if view.annotation === currentUserAnnotation {
+                mapView.selectAnnotation(view.annotation!, animated: true)
+            }
+
+            let point = MKMapPointForCoordinate(view.annotation!.coordinate)
+            let pointRect = MKMapRectMake(point.x, point.y, 0.1, 0.1)
+            zoomRect = MKMapRectUnion(zoomRect, pointRect)
+        }
+
+        mapView.setVisibleMapRect(zoomRect, animated: true)
+    }
 }
