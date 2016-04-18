cf912d37c9ae4c8dfc59f308c08c5daecd218ddb
Michael MacCallum <micksmaccallum@gmail.com>
2016-04-18 18:42:42 -0400

    Removed dead code

--- PRIVY/LoginViewController.swift
+++ PRIVY/LoginViewController.swift
@@ -237,16 +237,6 @@ final class LoginViewController: UIViewController {
     @IBAction private func switchingButtonTapped(button: UIButton!) {
         viewState = viewState == .Login ? .Registration : .Login
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
     @IBAction private func forgotPasswordButtonTapped(button: UIButton!) {
         let alertController = UIAlertController(
