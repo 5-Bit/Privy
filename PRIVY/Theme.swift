1d9819008282aa06d6f658ab2e408f3ca458d1dc
Michael MacCallum <micksmaccallum@gmail.com>
2016-04-17 22:03:18 -0400

    Files for theming added

---
+++ PRIVY/Theme.swift
@@ -0,0 +1,15 @@
+//
+//  Theme.swift
+//  Privy
+//
+//  Created by Michael MacCallum on 4/17/16.
+//  Copyright © 2016 Michael MacCallum. All rights reserved.
+//
+
+import UIKit
+
+struct Theme {
+    let primaryColor: UIColor
+    let secondaryColor: UIColor
+}
+
